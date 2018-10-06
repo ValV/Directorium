2bff40c3bf6c0fa35b12d8d9b123121ec8dd744a
Vladimir Valeyev <ValV@users.noreply.github.com>
2018-10-06 14:05:48 +0600

    Move bottom nodes to ToolPanelFragment

--- directorii-nucleus/src/main/kotlin/com/github/valv/directorium/view/MainView.kt
+++ directorii-nucleus/src/main/kotlin/com/github/valv/directorium/view/MainView.kt
@@ -6,9 +6,7 @@ import com.github.valv.directorium.control.Events.*
 import javafx.application.Platform
 import javafx.beans.value.ObservableValue
 import javafx.collections.ObservableList
-import javafx.geometry.Pos.*
 import javafx.scene.control.*
-import javafx.scene.layout.Priority.*
 import tornadofx.*
 
 class MainView : View("Directorium") {
@@ -38,38 +36,7 @@ class MainView : View("Directorium") {
             }
         }
         bottom {
-            vbox {
-                hbox {
-                    toolbar {
-                        subscribe<CommandResize> { prefWidth = it.number / 4 }
-                        padding = insets(4)
-                        button("◪✔") {
-                            action { fire(CommandCreateSection) }
-                        }
-                        button("◪✘") {
-                            action { fire(CommandDeleteSection) }
-                        }
-                    }
-                    toolbar {
-                        padding = insets(4)
-                        alignment = BASELINE_RIGHT
-                        hgrow = ALWAYS
-                        button("◨✔") { action { fire(CommandCreateField) } } //▥▾↴⊕⊖▣◨✔✘
-                        button("◨✘") { action { fire(CommandDeleteField) } }
-                        region { hgrow = ALWAYS }
-                        button("⬓✔") { action { fire(CommandCreateRecord) } } //▤▸↳⊕⊖▣⬓✔✘
-                        button("⬓✘") { action { fire(CommandDeleteRecord) } }
-                        region { hgrow = ALWAYS }
-                        button("⏻") { action { fire(CommandQuit) } }
-                    }
-                }
-                hbox {
-                    style { alignment = BASELINE_RIGHT }
-                    label {
-                        subscribe<CommandStatusDisplay> { text = it.status }
-                    }
-                }
-            }
+            add(ToolPanelFragment::class)
         }
     }
 
---
+++ directorii-nucleus/src/main/kotlin/com/github/valv/directorium/view/ToolPanelFragment.kt
@@ -0,0 +1,41 @@
+package com.github.valv.directorium.view
+
+import com.github.valv.directorium.control.Events.*
+import javafx.geometry.Pos.*
+import javafx.scene.layout.Priority.*
+import tornadofx.*
+
+class ToolPanelFragment : Fragment() {
+    override val root = vbox {
+        hbox {
+            toolbar {
+                subscribe<CommandResize> { prefWidth = it.number / 4 }
+                padding = insets(4)
+                button("◪✔") {
+                    action { fire(CommandCreateSection) }
+                }
+                button("◪✘") {
+                    action { fire(CommandDeleteSection) }
+                }
+            }
+            toolbar {
+                padding = insets(4)
+                alignment = BASELINE_RIGHT
+                hgrow = ALWAYS
+                button("◨✔") { action { fire(CommandCreateField) } }
+                button("◨✘") { action { fire(CommandDeleteField) } }
+                region { hgrow = ALWAYS }
+                button("⬓✔") { action { fire(CommandCreateRecord) } }
+                button("⬓✘") { action { fire(CommandDeleteRecord) } }
+                region { hgrow = ALWAYS }
+                button("⏻") { action { fire(CommandQuit) } }
+            }
+        }
+        hbox {
+            style { alignment = BASELINE_RIGHT }
+            label {
+                subscribe<CommandStatusDisplay> { text = it.status }
+            }
+        }
+    }
+}
